8125c1cf8e6c5deb92e37201b7edbcc497b36867
mxblsdl <maxblasdel@gmail.com>
2021-06-09 20:30:51 -0700

    skeleton of main page

--- main/app.R
+++ main/app.R
@@ -1,3 +1,20 @@
 # main site
 # 
 # 
+library(shiny)
+library(shinyjs)
+library(shinythemes)
+
+ui <- fluidPage(theme = shinytheme("superhero"),
+  shinythemes::themeSelector()
+
+)
+
+server <- function(input, output, session){
+  
+}
+
+shinyApp(ui, server)
+
+
+
