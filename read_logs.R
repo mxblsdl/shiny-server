160fe6ef36725420bb07bea9f9dd941bbd23f8fc
mxblsdl <maxblasdel@gmail.com>
2021-07-12 20:47:55 -0700

    move files

--- read_logs.R
+++ read_logs.R
@@ -5,22 +5,32 @@ library(ssh)
 shiny_log <- function(host) {
   max <-
     ssh_connect(host, passwd = Sys.getenv("ssh_pswd"))
+
+  # retrieve date modified info
+  logs <-
+    ssh_exec_internal(max, command = "stat -c %y /var/log/shiny-server/*")
   
+  l <- paste(rawToChar(logs$stdout))
+  l <- unlist(strsplit(l, "\n"))
+
+  # convert to datetime and find max
+  date_mod <- as.POSIXct(l)
+  ind <-which.max(date_mod)
+  
+  # read files 
   logs <-
     ssh_exec_internal(max, command = "ls /var/log/shiny-server/")
   
   l <- paste(rawToChar(logs$stdout))
-  cat(l)
-# issue with function ordering things alphabetically, 
-# makes it harder to find most recent file
   l <- unlist(strsplit(l, "\n"))
-  
+
+  # echo most recently modified file
   ssh_exec_wait(max,
                 command = paste0(
                   "echo ",
                   Sys.getenv("ssh_pswd"),
                   " | sudo -S cat /var/log/shiny-server/",
-                  l[length(1)]
+                  l[ind]
                 ))
   ssh_disconnect(max)
 }
