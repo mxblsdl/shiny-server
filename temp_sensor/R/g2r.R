17b6478b88cc056961e125c9de97e400034ed8b3
mxblsdl <maxblasdel@gmail.com>
2021-10-17 13:43:48 -0700

    change plotting function

--- temp_sensor/R/g2r.R
+++ temp_sensor/R/g2r.R
@@ -1,7 +1,7 @@
 # Plotting function
 g2plot <- function(x, metric) {
   x %>% 
-    dplyr::mutate(interval = strftime(interval, format = "%m-%d %I:%M %p")) %>%
+    dplyr::mutate(interval = format(interval, format = "%m-%d %I:%M %p")) %>%
     g2(., asp(interval, .data[[metric]])) %>% 
     fig_line() %>% 
     fig_area() %>% 
