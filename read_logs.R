6d45cd08952565870e3ae12e4fd74dbcb3f1a85a
mxblsdl <maxblasdel@gmail.com>
2022-03-04 20:30:14 -0800

    change env vars

--- read_logs.R
+++ read_logs.R
@@ -3,12 +3,12 @@
 library(ssh)
 
 shiny_log <- function(host) {
-  max <-
+  con <-
     ssh_connect(host, passwd = Sys.getenv("ssh_pswd"))
 
   # retrieve date modified info
   logs <-
-    ssh_exec_internal(max, command = "stat -c %y /var/log/shiny-server/*")
+    ssh_exec_internal(con, command = "stat -c %y /var/log/shiny-server/*")
   
   l <- paste(rawToChar(logs$stdout))
   l <- unlist(strsplit(l, "\n"))
@@ -19,20 +19,20 @@ shiny_log <- function(host) {
   
   # read files 
   logs <-
-    ssh_exec_internal(max, command = "ls /var/log/shiny-server/")
+    ssh_exec_internal(con, command = "ls /var/log/shiny-server/")
   
   l <- paste(rawToChar(logs$stdout))
   l <- unlist(strsplit(l, "\n"))
 
   # echo most recently modified file
-  ssh_exec_wait(max,
+  ssh_exec_wait(con,
                 command = paste0(
                   "echo ",
                   Sys.getenv("ssh_pswd"),
                   " | sudo -S cat /var/log/shiny-server/",
                   l[ind]
                 ))
-  ssh_disconnect(max)
+  ssh_disconnect(con)
 }
 
-shiny_log("max@167.71.158.245")
+shiny_log(paste0(Sys.getenv("ssh_host"), '@167.71.158.245'))
